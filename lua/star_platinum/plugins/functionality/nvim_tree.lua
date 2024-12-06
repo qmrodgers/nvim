be77e1f56da9d66817b5a11f9c049afccc57e5e6
Quaid <business.quaidrodgers@gmail.com>
2024-12-05 22:44:47 -0600

    modify oil and nvim_tree mappings to better sync cwd across plugins

--- lua/star_platinum/plugins/functionality/nvim_tree.lua
+++ lua/star_platinum/plugins/functionality/nvim_tree.lua
@@ -1,24 +1,43 @@
 return {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        require("nvim-tree").setup({
-          sort = {
-            sorter = "case_sensitive",
-          },
-          view = {
-            width = 30,
-          },
-          renderer = {
-            group_empty = true,
-          },
-          filters = {
-            dotfiles = true,
-          },
-        })
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local function attach(bufnr)
+			vim.g.tree_initial_cwd = vim.fn.getcwd()
+			local api = require("nvim-tree.api")
 
-        vim.keymap.set("n", "\\", "<Cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
-    end
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+
+			-- custom mappings
+			--
+			vim.keymap.set("n", "_", function()
+				api.tree.change_root(vim.g.tree_initial_cwd)
+			end, opts("Reset to CWD"))
+		end
+
+		require("nvim-tree").setup({
+			on_attach = attach,
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 30,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		})
+
+		vim.keymap.set("n", "\\", "<Cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
+	end,
 }
--- lua/star_platinum/plugins/functionality/oil.lua
+++ lua/star_platinum/plugins/functionality/oil.lua
@@ -1,18 +1,25 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        require('oil').setup({
-            columns = { "icon", "size", "time" },
-            keymaps = {
-                ["<C-h>"] = false,
-                ["<M-h>"] = "actions.select_split"
-            },
-            view_options = {
-                show_hidden = true,
-            }
-        })
-        vim.keymap.set("n", "-", "<cmd>Oil<CR>")
-        vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
-    end
+	"stevearc/oil.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("oil").setup({
+			columns = { "icon", "size", "time" },
+			keymaps = {
+				["<C-h>"] = false,
+				["<M-h>"] = "actions.select_split",
+				["~"] = {
+					callback = function()
+						require("oil.actions").cd.callback()
+						vim.g.tree_initial_cwd = vim.fn.getcwd()
+						require("nvim-tree.api").tree.change_root(vim.g.tree_initial_cwd)
+					end,
+				},
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
+		vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+		vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+	end,
 }
