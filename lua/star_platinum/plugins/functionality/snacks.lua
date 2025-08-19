334f352053203187815cb2e1509846c418947b8d
Quaid <business.quaidrodgers@gmail.com>
2025-08-18 20:11:39 -0500

    🔧(neovim): modify snacks pickers, allow visual selection word grep

--- lua/star_platinum/plugins/functionality/snacks.lua
+++ lua/star_platinum/plugins/functionality/snacks.lua
@@ -8,7 +8,7 @@ return {
     -- { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
-    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>/", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
     { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
     -- { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
     { "<leader>e", function() Snacks.explorer({ hidden = true, ignored = true }) end, desc = "File Explorer" },
@@ -32,7 +32,7 @@ return {
     { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
     { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
     { "<leader>sg", function() Snacks.picker.grep({ hidden = true }) end, desc = "Grep" },
-    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "v", "x" } },
     -- -- search
     { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
     { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
@@ -76,7 +76,7 @@ return {
     -- input = { enabled = true },
     picker = {
       enabled = true,
-      focus = "list",
+      focus = "input",
       jump = {
         reuse_win = true,
       },
